7bda6444c8a9292fb60b5ccc9d9ec3c2429c4724
Eli Ennab <elinaennab@gmail.com>
2023-08-22 20:44:20 +0200

    implement react query dev tools in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 const App = () => {
 
 return (
         <>
+
+            <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
         </>
     )
 }
