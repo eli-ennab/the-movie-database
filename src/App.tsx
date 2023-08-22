534308f285836f70d2c8aa2d678707a7dd41a924
Eli Ennab <elinaennab@gmail.com>
2023-08-22 20:59:21 +0200

    preparing for future routes and added simple home page and page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import PageNotFound from './pages/PageNotFound'
+
 import './assets/scss/App.scss'
 
 const App = () => {
 
 return (
         <>
+                <Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
 
             <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
         </>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+	return (
+		<>
+			<h1>The Movie DB</h1>
+		</>
+	)
+}
+
+export default HomePage
