102643a3a207575a5e812d6b9110692d6f2acb53
Eli Ennab <elinaennab@gmail.com>
2023-08-23 12:20:39 +0200

    cards with placeholder image for each movie

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { getMovieGenre } from '../services/TheMovieDB_API'
-import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
 
 const GenrePage = () => {
 
@@ -9,23 +12,27 @@ const GenrePage = () => {
         queryKey: ['genre']
     })
 
-    console.log(data?.results)
-
     return (
         <>
-            <h1>Which genre?</h1>
+            <h1 className="py-4">Which genre?</h1>
 
             { data?.results && (
-                <ListGroup>
-                    {data.results.map (genre => (
-                        <ListGroup.Item
-                            key={genre.id}
-                        > 
-                            name '{genre.original_title}'', id '{genre.id}'
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-            )} 
+            <Row xs={1} md={3} lg={5} className="g-4">
+                {data.results.map(movie => (
+                    <Col key={movie.id}>
+                        <Card>
+                            <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/gwrCRHLDbjdrvIVRCPg8FB8FRMH.jpg" />
+                            <Card.Body>
+                            <Card.Title>{movie.title}</Card.Title>
+                            <Card.Text>
+                                Popularity: {movie.popularity}
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            )}
         </>
     )
 }
