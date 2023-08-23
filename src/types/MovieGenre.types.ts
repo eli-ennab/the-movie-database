aa80aee7381c1abb4ae68be7907e2e48824f0c12
Eli Ennab <elinaennab@gmail.com>
2023-08-23 11:58:43 +0200

    added image paths to types

--- src/types/MovieGenre.types.ts
+++ src/types/MovieGenre.types.ts
@@ -7,6 +7,7 @@ export type MovieGenreResponse = {
 
 export type GenreResult = {
     adult: false,
+    backdrop_path: string | null
     genre_ids: [
         number,
         number
@@ -16,6 +17,7 @@ export type GenreResult = {
     original_title: string
     overview: string
     popularity: number
+    poster_path: string | null
     release_date: Date
     title: string
     video: boolean
