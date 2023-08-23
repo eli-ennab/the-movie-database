5244e1dfd3ae08f2c986cb13770a69aa3c7f81fe
Eli Ennab <elinaennab@gmail.com>
2023-08-23 10:58:44 +0200

    individual genre as Link and direct to future GenrePage

--- src/pages/GenresPage.tsx
+++ src/pages/GenresPage.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { getMovieGenres } from '../services/TheMovieDB_API'
+import { Link } from 'react-router-dom'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const GenresPage = () => {
@@ -11,20 +12,22 @@ const GenresPage = () => {
 
     return (
         <>
+            <h1 className="py-4">All Genres</h1>
 
-        <h1 className="py-4">All Genres</h1>
-
-        { getGenres.data && (
-            <ListGroup>
-                {getGenres.data.genres.map (genre => (
-                    <ListGroup.Item
-                        key={genre.id}
-                    > 
-                        {genre.name}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-        )} 
+            { getGenres.data && (
+                <ListGroup>
+                    {getGenres.data.genres.map (genre => (
+                        <ListGroup.Item
+                            key={genre.id}
+                            action
+                            as={Link}
+                            to={`/genres/${genre.id}`}
+                        > 
+                            {genre.name}
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            )} 
         </>
     )
 }
