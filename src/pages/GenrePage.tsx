89077b8309dd468c4af35a7bafc3d3f7f25b04ae
Eli Ennab <elinaennab@gmail.com>
2023-08-23 13:14:51 +0200

    getting the matching image for each movie

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -21,7 +21,7 @@ const GenrePage = () => {
                 {data.results.map(movie => (
                     <Col key={movie.id}>
                         <Card>
-                            <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/gwrCRHLDbjdrvIVRCPg8FB8FRMH.jpg" />
+                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                             <Card.Body>
                             <Card.Title>{movie.title}</Card.Title>
                             <Card.Text>
