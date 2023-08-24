e04f760b4dc460cedf0426ab357df45fd55c6633
Eli Ennab <elinaennab@gmail.com>
2023-08-24 15:16:38 +0200

    new route to movie/:id

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import GenrePage from './pages/GenrePage'
 import GenresPage from './pages/GenresPage'
+import MoviePage from './pages/MoviePage'
 import PageNotFound from './pages/PageNotFound'
 import Navigation from './components/Navigation'
 import Container from 'react-bootstrap/Container'
@@ -19,6 +20,7 @@ const App = () => {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/genres" element={<GenresPage />} />
                         <Route path="/genres/:id" element={<GenrePage />} />
+                        <Route path="/movies/:id" element={<MoviePage />} />
                         <Route path="*" element={<PageNotFound />} />
                     </Routes>
                 </Container>
