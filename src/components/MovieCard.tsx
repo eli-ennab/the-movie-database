9a7c0d3e6e314c075cc7069a12195016fbd3b237
Eli Ennab <elinaennab@gmail.com>
2023-08-24 15:17:02 +0200

    redirecting to movie/:id

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,22 +1,30 @@
+import { useNavigate } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 interface IProps {
     poster_path: string | null
     title: string
+    id: number
 }
 
-const MovieCard: React.FC<IProps> = ({ poster_path, title }) => {
+const MovieCard: React.FC<IProps> = ({ poster_path, title, id }) => {
+    const navigate = useNavigate()
     const URL = "https://image.tmdb.org/t/p/w500"
+
+    const redirectToMovie = (id: number) => {
+        navigate(`/movies/${id}`, { replace: true })
+      }
 
     return (
         <Card>
-            <Card.Img variant="top"  src={ URL + poster_path } />
+            <Card.Img variant="top" src={ URL + poster_path } />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <div className="d-grid gap-2">
                     <Button
                         variant="dark"
+                        onClick={() => redirectToMovie(id)}
                     >
                             Read more
                     </Button>
