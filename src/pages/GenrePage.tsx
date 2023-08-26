fee89f4822c48667bbd188c82dafea511ac6f788
Eli Ennab <elinaennab@gmail.com>
2023-08-26 15:35:07 +0200

    using the pagination that so far is the best, all tho not properly working

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -1,17 +1,18 @@
+import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { getMovieGenre } from '../services/TheMovieDB_API'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import MovieCard from '../components/MovieCard'
-// import Pagination from '../components/Pagination'
+import Pagination from '../components/Pagination'
 
 const GenrePage = () => {
     const { id } = useParams()
     const genreId = Number(id)
 
-    const [page, setPage] = useState(1)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const page = Number(searchParams.get('page')) || 1
 
     const {
         data,
@@ -20,28 +21,35 @@ const GenrePage = () => {
         isFetching,
         isPreviousData,
     } = useQuery({
-        queryKey: ['genre', genreId, page],
+        queryKey: ['genre', { genreId: genreId }, { currentPage: page }],
         queryFn: () => getMovieGenre(genreId, page),
         keepPreviousData : true
-      })
+    })
     
+    useEffect(() => {
+        searchParams.set('page', page.toString())
+        setSearchParams(searchParams)
+    }, [page, searchParams, setSearchParams])
+
     if (data === undefined) {
         return
     }
 
+    console.log("is previous data:", isPreviousData)
+
     return (
         <>
-            <h1 className="py-4">Which genre?</h1>
+            <h1 className="py-4"></h1>
 
             <p>Showing {data.results.length} results out of {data.total_results}</p>
+
+            { isError && (
+                <p>Error</p>
+            )}
 
             { isLoading && (
                 <p>Loading</p>
             )}
-
-            { isError && (
-                <p>Error</p>
-            )} 
 
             { data.results && (
                 <Row xs={1} md={3} lg={5} className="g-4">
@@ -53,36 +61,18 @@ const GenrePage = () => {
                 </Row>
             )}
 
-            <span>Current Page: {page}</span>
+            { isFetching && (
+                <p>Fetching</p>
+            )}
 
-            <button
-            onClick={() => setPage(old => Math.max(old - 1, 0))}
-            disabled={page === 1}
-            >
-            Previous Page
-            </button>{' '}
-
-            <button
-            onClick={() => {
-                if (!isPreviousData && page + 1 > 0 ) {
-                setPage(old => old + 1)
-                }
-            }}
-            disabled={isPreviousData || page + 1 >= data.total_pages}
-            >
-            Next Page
-            </button>
-
-            {isFetching ? <span> Loading...</span> : null}{' '}
-
-            {/* <Pagination
-                page={1}
+            <Pagination
+                page={data.page}
                 totalPages={data.total_pages}
                 hasPreviousPage={data.page > 1}
-                hasNextPage={data.page < 1} 
-                onPreviousPage={} 
-                onNextPage={}
-            /> */}
+                hasNextPage={data.page < data.total_pages}
+                onPreviousPage={() => { setSearchParams( { page: (Number(page) - 1).toString() }) }}
+                onNextPage={() => { setSearchParams( { page: (Number(page) + 1).toString() }) }}
+			/>
         </>
     )
 }
