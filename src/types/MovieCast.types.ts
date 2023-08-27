d1d7d86563436cb10c9dee9d0d4c5bb5b0b8be1a
Eli Ennab <elinaennab@gmail.com>
2023-08-27 20:50:36 +0200

    new type: Actor

--- src/types/MovieCast.types.ts
+++ src/types/MovieCast.types.ts
@@ -17,3 +17,24 @@ export type Cast = {
     credit_id: string
     order: number
 }
+
+export type Actor = {
+    adult: boolean
+    also_known_as: AlsoKnownAsList[]
+    biography: string
+    birthday: string
+    deathday: string
+    gender: number
+    homepage: string | null
+    id: number
+    imdb_id: string
+    known_for_department: string
+    name: string
+    place_of_birth: string
+    popularity: number
+    profile_path: string
+}
+
+export type AlsoKnownAsList = {
+    also_known_as: string
+}
