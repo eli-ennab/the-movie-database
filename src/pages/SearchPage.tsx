52b54f1eb9ca8d86030346a468c0385293b5e8ae
Eli Ennab <elinaennab@gmail.com>
2023-08-28 15:30:26 +0200

    indentation

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -8,9 +8,17 @@ const SearchPage = () => {
             <Form className="form-wrapper">
                 <Form.Group controlId="searchQuery">
                 <Form.Label>Search for movies</Form.Label>
-                    <Form.Control type="text" className="search-form" placeholder="Enter your search" />
+                    <Form.Control 
+                        className="search-form" 
+                        type="text" 
+                        placeholder="Enter your search"
+                    />
                 </Form.Group>
-                <Button className="submit-search-btn py-2 mx-2" variant="dark" type="submit">
+                <Button 
+                    className="submit-search-btn py-2 mx-2" 
+                    variant="dark" 
+                    type="submit"
+                >
                     Search
                 </Button>
             </Form>
