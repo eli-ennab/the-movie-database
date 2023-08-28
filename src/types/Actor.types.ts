b4ea72cb1b2d97c67551e5a44c443b699f333bde
Eli Ennab <elinaennab@gmail.com>
2023-08-28 15:00:22 +0200

    extended actor type with movie_credits

--- src/types/Actor.types.ts
+++ src/types/Actor.types.ts
@@ -13,6 +13,30 @@ export type Actor = {
     place_of_birth: string
     popularity: number
     profile_path: string
+    movie_credits: {
+        cast: ActorCast[]
+    }
+
+}
+
+export type ActorCast = {
+    adult: boolean
+    backdrop_path: string
+    genre_ids: number[]
+    id: number
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+    character: string
+    credit_id: string
+    order: number
 }
 
 export type AlsoKnownAsList = {
