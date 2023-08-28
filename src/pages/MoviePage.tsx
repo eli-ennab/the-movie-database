895ce45ab432b03b90a53309e91be9845e2e6445
Eli Ennab <elinaennab@gmail.com>
2023-08-28 18:02:02 +0200

    perfecting movie page

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import IsErrorAlert from '../components/IsErrorAlert'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const MoviePage = () => {
@@ -34,21 +35,25 @@ const MoviePage = () => {
     return (
         <>
             <Card>
-                <Card.Img variant="top" src={ URL + data.backdrop_path } className="w-25 fluid" />
-                    <Card.Body>
+                <Card.Body>
+                    <Image src={ URL + data.backdrop_path } className="mb-4 border-img" fluid />
                         <Card.Title>{data.title}</Card.Title>
+                        <Card.Text>Release date: {data.release_date}</Card.Text>
+                        <Card.Text><span className="vote-average">{data.vote_average}</span></Card.Text>
                         <Card.Text>{data.overview}</Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroup.Item>Original language: {data.original_language}</ListGroup.Item>
-                            <ListGroup.Item>Original title: {data.original_title}</ListGroup.Item>
-                            <ListGroup.Item>Popularity: {data.popularity}</ListGroup.Item>
-                        </ListGroup>
+                        <Card.Text>Tagline: "{data.tagline}"</Card.Text>
+                        <Card.Text>Original language: {data.original_language}</Card.Text>
+                        <Card.Text>Original title: {data.original_title}</Card.Text>
+                        <Card.Text>Runtime: {data.runtime} mins</Card.Text>
+                        <Card.Text>Popularity: {data.popularity}</Card.Text>
+                        <Card.Text>Budget: {data.budget}</Card.Text>
                         <hr></hr>
-                        <h2>Actors</h2>
+                        <h2 className="mb-4">Actors</h2>
                         { data.credits.cast && (
-                            <ListGroup>
+                            <ListGroup className="list-group-sm">
                                 {actors?.map (actor => (
                                     <ListGroup.Item
+                                        className="list-item sm"
                                         key={actor.id}
                                         action
                                         as={Link}
@@ -62,7 +67,7 @@ const MoviePage = () => {
 
                         <hr></hr>
                         
-                        <h3>If you like this movie, you might like...</h3>
+                        <h3 className="h2 mb-4">If you like {data.title}, you might like..</h3>
                             <Card.Link href="#">Movie 1</Card.Link>
                             <Card.Link href="#">Movie 2</Card.Link>
                     </Card.Body>
