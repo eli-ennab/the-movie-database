07139aa438deb7f96be4006844de8a1356d9f60e
Eli Ennab <elinaennab@gmail.com>
2023-08-28 11:26:07 +0200

    organizing code

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
-import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 interface IProps {
     poster_path: string | null
