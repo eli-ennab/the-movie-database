91eb74cf01060f2805bbc80c007aa9b8eaece462
Eli Ennab <elinaennab@gmail.com>
2023-08-28 18:02:46 +0200

    actor page also looking better, render all info, movies that are linked to movie page

--- src/pages/ActorPage.tsx
+++ src/pages/ActorPage.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { getActor } from '../services/TheMovieDB_API'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import IsErrorAlert from '../components/IsErrorAlert'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const ActorPage = () => {
@@ -31,26 +32,32 @@ const ActorPage = () => {
     return (
         <>
             <Card>
-                <Card.Img variant="top" src={ URL + data.profile_path } className="w-25 fluid" />
+                <Card.Body>
+                    <Image src={ URL + data.profile_path } className="w-25 border-img" fluid />
                         <Card.Title>{data.name}</Card.Title>
+                        <Card.Text>Popularity: {data.popularity}</Card.Text>
                         <Card.Text>{data.biography}</Card.Text>
-                        <ListGroup className="list-group-flush">
-                            <ListGroup.Item>Birthday: {data.birthday}</ListGroup.Item>
-                            <ListGroup.Item>Place of birth: {data.place_of_birth}</ListGroup.Item>
-                        </ListGroup>
+                        <Card.Text>Birthplace: {data.place_of_birth}</Card.Text>
+                        <Card.Text>Birthday: {data.birthday}</Card.Text>
+                        <Card.Text>Deathday: {data.deathday}</Card.Text>
                         <hr></hr>
-                        <h2>Casting in:</h2>
+                        <h2 className="mb-4">Movies</h2>
                         { data.movie_credits.cast && (
-                            <ListGroup>
+                            <ListGroup className="list-group-sm">
                                 {data.movie_credits.cast.map (movie => (
                                     <ListGroup.Item
+                                        className="list-item sm"
                                         key={movie.id}
+                                        action
+                                        as={Link}
+                                        to={`/movies/${movie.id}`}
                                     > 
                                         {movie.title}
                                     </ListGroup.Item>
                                 ))}
                             </ListGroup>
                         )} 
+                    </Card.Body>
             </Card>
         </>
     )
