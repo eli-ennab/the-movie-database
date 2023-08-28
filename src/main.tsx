a87ac45dacb9ff2a493b4ef015b1ba774917a613
Eli Ennab <elinaennab@gmail.com>
2023-08-28 10:32:33 +0200

    added 10 mins staleTime and 15 mins cacheTime, and no refetch on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,15 @@ import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 10 * (60 * 1000), 
+			cacheTime: 15 * (60 * 1000),
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
