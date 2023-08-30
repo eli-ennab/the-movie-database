abe7c23f25e7c500ada6d951a265c708079002a8
Eli Ennab <elinaennab@gmail.com>
2023-08-30 10:05:55 +0200

    margin for error alert

--- src/components/IsErrorAlert.tsx
+++ src/components/IsErrorAlert.tsx
@@ -2,8 +2,8 @@ import Alert from 'react-bootstrap/Alert'
 
 const IsErrorAlert = () => {
     return (
-        <Alert variant="warning">
-            <h1>Something went wrong.</h1>
+        <Alert variant="warning my-4">
+            <h1 className="h2">Something went wrong.</h1>
         </Alert>
     )
 }
