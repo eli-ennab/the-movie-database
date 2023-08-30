e09bec212dd522f87793db016ff04e89cef4be00
Eli Ennab <elinaennab@gmail.com>
2023-08-30 17:18:25 +0200

    must have had removed w-25 on backdrop path image by mistake before, added again

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -56,7 +56,7 @@ const MovieCard: React.FC<IProps> = (
     return (
         <Card className="movie-card">
         <Card.Body>
-            <Image src={backdrop_path ? URL + backdrop_path : na_image} className="mb-4 border-img" fluid />
+            <Image src={backdrop_path ? URL + backdrop_path : na_image} className="w-25 mb-4 border-img" fluid />
                 <Card.Title>{title || 'N/A'}</Card.Title>
                 <Card.Text>Release date: {release_date || 'N/A'}</Card.Text>
                 <Card.Text><span className="vote-average">{vote_average || 'N/A'}</span></Card.Text>
