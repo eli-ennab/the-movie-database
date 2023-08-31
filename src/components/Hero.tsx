56e93b0e9e30f0a2f1b09ba36de2add7e4fea936
Eli Ennab <elinaennab@gmail.com>
2023-08-31 08:58:16 +0200

    adding another option for hero image to try out, not decided which yet

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import hero_image from '../images/samuel-regan-asante-wMkaMXTJjlQ-unsplash.jpg'
+// import hero_image_2 from '../images/samuel-regan-asante-wMkaMXTJjlQ-unsplash_2.jpg'
 import Image from 'react-bootstrap/Image'
 
 const Hero = () => {
@@ -6,14 +7,16 @@ const Hero = () => {
         <>
             <div className="hero-image">
             <div className="hero-text">
-                <h2>
-                    Your movie database. 
+                <h2 className="mb-4">
+                    <span className="hero-header text-border">
+                        Your movie database. 
+                    </span>
                 </h2>
-                <a href="#top-rated">
-                    Top Rated
-                </a>
                 <a href="#trending">
                     Trending
+                </a>
+                <a href="#top-rated">
+                    Top Rated
                 </a>
                 <a href="#now-playing">
                     Now Playing
@@ -21,6 +24,7 @@ const Hero = () => {
             </div>
             <div className="image-container">
                 <Image src={hero_image} fluid />
+                {/* <Image src={hero_image_2} fluid /> */}
             </div>
         </div>
         </>
