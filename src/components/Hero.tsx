e8ae719fc271b4759b93819e555808e3d5426afa
Eli Ennab <elinaennab@gmail.com>
2023-08-31 09:47:24 +0200

    indentation and sorting/cleaning in css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,27 +6,27 @@ const Hero = () => {
     return (
         <>
             <div className="hero-image">
-            <div className="hero-text">
-                <h2 className="mb-4">
-                    <span className="hero-header text-border">
-                        Your movie database. 
-                    </span>
-                </h2>
-                <a href="#trending">
-                    Trending
-                </a>
-                <a href="#top-rated">
-                    Top Rated
-                </a>
-                <a href="#now-playing">
-                    Now Playing
-                </a>
+                <div className="hero-text">
+                    <h2 className="mb-4">
+                        <span className="hero-header text-border">
+                            Your movie database. 
+                        </span>
+                    </h2>
+                    <a href="#trending">
+                        Trending
+                    </a>
+                    <a href="#top-rated">
+                        Top Rated
+                    </a>
+                    <a href="#now-playing">
+                        Now Playing
+                    </a>
+                </div>
+                <div className="image-container">
+                    <Image src={hero_image} fluid />
+                    {/* <Image src={hero_image_2} fluid /> */}
+                </div>
             </div>
-            <div className="image-container">
-                <Image src={hero_image} fluid />
-                {/* <Image src={hero_image_2} fluid /> */}
-            </div>
-        </div>
         </>
     )
 }
