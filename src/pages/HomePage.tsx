97073fff2d9ffec03c5bd3257d6e24b79a70f928
Eli Ennab <elinaennab@gmail.com>
2023-08-31 16:31:33 +0200

    .. movie objects that i can get and render in my homepage!

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react'
 import { useIsFetching } from '@tanstack/react-query'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
 import Hero from '../components/Hero'
 import IsErrorAlert from '../components/IsErrorAlert'
 import MovieInListCard from '../components/MovieInListCard'
 import useTopRatedMovies from '../hooks/useTopRatedMovies'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import useTrendingMovies from '../hooks/useTrendingMovies'
+import { MovieResponse } from '../types/Movie.types'
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const HomePage = () => {
     const [timeWindow, setTimeWindow] = useState<string>('day')
@@ -27,6 +28,9 @@ const HomePage = () => {
     if (topRatedMovies.isError || nowPlayingMovies.isError || trendingMovies.isError) {
 		return <IsErrorAlert />
 	}
+
+    const jsonMovies = localStorage.getItem('movies') ?? '[]'
+    const latestMovies: MovieResponse[] = JSON.parse(jsonMovies)
 
 	return !isFetching ? (
 		<>
@@ -99,6 +103,28 @@ const HomePage = () => {
 			{ nowPlayingMovies.data && nowPlayingMovies.data.results && (
                 <Row xs={1} md={3} lg={5} className="g-4">
                     {nowPlayingMovies.data.results.slice(0, 10).map(movie => (
+                        <Col key={movie.id}>
+                            <MovieInListCard 
+                                poster_path={movie.poster_path} 
+                                title={movie.title} 
+                                id={movie.id} 
+                                vote_average={movie.vote_average}
+                                release_date={movie.release_date} 
+                            />
+                        </Col>
+                    ))}
+                </Row>
+            )}
+
+            <h2 id="now-playing" className="my-5">
+                <span className="text-border">
+                    Recent movies you watched
+                </span>
+			</h2>
+
+            { latestMovies && (
+                <Row xs={1} md={3} lg={5} className="g-4">
+                    {latestMovies.slice(-10).map(movie => (
                         <Col key={movie.id}>
                             <MovieInListCard 
                                 poster_path={movie.poster_path} 
